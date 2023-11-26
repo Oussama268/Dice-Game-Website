f069fc4939f598978e96d3d279809003490767a7
oussama2680 <oussamaafs268@gmail.com>
2023-11-26 18:59:49 +0100

    the dice module is added

--- src/App.js
+++ src/App.js
@@ -1,24 +1,72 @@
-import logo from './logo.svg';
 import './App.css';
 
+import * as THREE from 'three'
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
+
+
+
+
+
+
 function App() {
+/*
+  const scene = new THREE.Scene();
+  const camera = new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight, 0.1, 1000 );
+
+
+
+
+
+
+  const renderer = new THREE.WebGLRenderer();
+  renderer.setSize( window.innerWidth, window.innerHeight );
+  renderer.setClearColor(0xA3A3A3)
+
+  document.body.appendChild( renderer.domElement );
+
+  camera.position.set(10,10,10)
+
+
+  const loader = new GLTFLoader();
+
+  loader.load( './Dice.glb', function ( gltf ) {
+    const model = gltf.scene
+    scene.add( model );
+
+  }, undefined, function ( error ) {
+
+  console.error( error );
+
+  } );
+
+
+
+
+
+
+  function animate() {
+
+    renderer.render( scene, camera );
+  }
+
+  
+
+  renderer.setAnimationLoop(animate)
+
+
+*/
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+   
+
+    <h1>Hello</h1>
+    
+    
+    </>
+    
+   
   );
 }
 
